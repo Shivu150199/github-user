198cbadd3d02c64dc66063894059b0abb62445f7
shivam singh gautam <shivamsinghgautam397@gmail.com>
2023-06-22 04:43:20 +0530

    initial commit

---
+++ src/context/context.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react'
+import mockUser from './mockData.js/mockUser'
+import mockRepos from './mockData.js/mockRepos'
+import mockFollowers from './mockData.js/mockFollowers'
+import axios from 'axios'
+
+const rootUrl = 'https://api.github.com'
+
+const GithubContext = React.createContext()
+
+const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser)
+  const [repos, setRepos] = useState(mockRepos)
+  const [followers, setFollowers] = useState(mockFollowers)
+
+  const [requests, setRequests] = useState(0)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const [error, setError] = useState({ show: false, msg: '' })
+
+  const searchGithubUser = async (user) => {
+    console.log(user)
+    toggleError()
+    setIsLoading(true)
+    const response = await axios(`${rootUrl}/users/${user}`).catch((err) =>
+      console.log(err)
+    )
+    if (response) {
+      setGithubUser(response.data)
+      const {login,followers_url}=response.data;
+      axios(`${rootUrl}/users/${login}/repos?per_page=100`).then((response)=>setRepos(response.data))
+      
+      axios(`${followers_url}?per_page=100`).then((response)=>setFollowers(response.data))
+
+    } else {
+      toggleError(true, 'there is no user with  user name')
+    }
+    checkRequests()
+    setIsLoading(false)
+  }
+
+  const checkRequests = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        let {
+          rate: { remaining },
+        } = data
+        setRequests(remaining)
+        if (remaining === 0) {
+          //throw error
+          toggleError(true, `sorry ,you have exceed the hourly rate limit`)
+        }
+      })
+      .catch((err) => console.log(err))
+  }
+  function toggleError(show, msg) {
+    setError({ show, msg })
+  }
+  useEffect(checkRequests, [])
+
+  return (
+    <GithubContext.Provider
+      value={{
+        githubUser,
+        repos,
+        followers,
+        requests,
+        error,
+        searchGithubUser,
+        isLoading,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  )
+}
+
+export { GithubProvider, GithubContext }
---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import { GithubProvider } from './context/context'
+import { Auth0Provider } from '@auth0/auth0-react'
+const root = ReactDOM.createRoot(document.getElementById('root'))
+// dev-2df0bvtgrysecn2t.us.auth0.com
+// client id=drAwaOxBdb0hhzfe3e4fHq7VR8WR3pgO
+root.render(
+  <React.StrictMode>
+    <Auth0Provider
+      domain="dev-2df0bvtgrysecn2t.us.auth0.com"
+      clientId="drAwaOxBdb0hhzfe3e4fHq7VR8WR3pgO"
+      redirectUri={window.location.origin}
+      cacheLocation='localstorage'
+
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
+  </React.StrictMode>
+)
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister()
